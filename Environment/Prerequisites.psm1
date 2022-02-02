eede011b9d3ada813f3010333a2c0b81ab677110
Eric Stockdale <estockdale@unicon.net>
2022-02-02 11:20:41 -0700

    [ODS-5257]Upgrade AppCommon to verify .net 6
    Signed-off-by: Eric Stockdale <estockdale@unicon.net>

--- Environment/Prerequisites.psm1
+++ Environment/Prerequisites.psm1
@@ -40,7 +40,7 @@ function Invoke-ThrowIfDotnetHostingBundleMissing {
 
     if(-Not $requiredVersionInstalled)
     {
-        throw "The .NET 6 Hosting Bundle couldn't be found on the system, and is required for install. 
+        throw ".NET 6 Hosting Bundle couldn't be found on the system, and is required for install. 
         Please install $requiredVersion or greater. Can be downloaded from https://dotnet.microsoft.com/en-us/download/dotnet/6.0"
     }
 }
