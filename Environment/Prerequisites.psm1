523d37c5ca6c5c8a1d2958ed38e99808863c75f8
Patrick McVeety-Mill <patrickm@headspring.com>
2022-04-13 14:29:11 -0500

    update link to generic all-version page

--- Environment/Prerequisites.psm1
+++ Environment/Prerequisites.psm1
@@ -42,8 +42,8 @@ function Invoke-ThrowIfDotnetHostingBundleMissing {
 
     if(-Not $requiredVersionInstalled)
     {
-        throw "The .NET 6 Hosting Bundle couldn't be found on the system, and is required for install. 
-        Please install $requiredVersion or greater. Can be downloaded from https://dotnet.microsoft.com/en-us/download/dotnet/6.0"
+        throw "The $VersionString .NET Hosting Bundle couldn't be found on the system, and is required for install.
+        Please install $requiredVersion or greater. Can be downloaded from https://dotnet.microsoft.com/en-us/download/dotnet"
     }
 }
 
