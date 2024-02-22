b1362cfe36d7d734cd3a5f7bff16723cb5b1b2fd
Stephen A. Fuqua <stephen@safnet.com>
2024-02-21 21:32:43 -0600

    new module

---
+++ github-builder.psm1
@@ -0,0 +1,52 @@
+# SPDX-License-Identifier: Apache-2.0
+# Licensed to the Ed-Fi Alliance under one or more agreements.
+# The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+# See the LICENSE and NOTICES files in the project root for more information.
+
+
+#requires -version 7
+
+$ErrorActionPreference = "Stop"
+
+function Get-VersionNumber {
+
+    $prefix = "v"
+
+    # Install the MinVer CLI tool
+    &dotnet tool install --global minver-cli
+
+    $version = $(minver -t $prefix)
+
+    "appcommon-v$version" >> $env:GITHUB_OUTPUT
+    "appcommon-semver=$($version -Replace $prefix)" >> $env:GITHUB_OUTPUT
+}
+
+function Invoke-DotnetPack {
+    [CmdletBinding()]
+    param (
+        [string]
+        [Parameter(Mandatory = $true)]
+        $Version
+    )
+
+    dotnet pack -p:PackageVersion=$Version -o ./
+}
+
+function Invoke-NuGetPush {
+    [CmdletBinding()]
+    param (
+        [string]
+        [Parameter(Mandatory = $true)]
+        $NuGetFeed,
+
+        [string]
+        [Parameter(Mandatory = $true)]
+        $NuGetApiKey
+    )
+
+    (Get-ChildItem -Path $_ -Name -Include *.nupkg) | ForEach-Object {
+        &dotnet nuget push $_ --api-key $NuGetApiKey --source $NuGetFeed
+    }
+}
+
+Export-ModuleMember -Function Get-VersionNumber, Invoke-DotnetPack
